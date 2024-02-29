e6fcce1ebbfe50958dca5550efa0c649be70ccde
Ryan Williams <ryan.williams@signalwire.com>
2024-02-28 21:23:21 -0800

    update info

--- lib/omniauth/strategies/signalwire.rb
+++ lib/omniauth/strategies/signalwire.rb
@@ -19,8 +19,15 @@ module OmniAuth
 
       info do
         {
-          name: raw_info["name"],
-          email: raw_info["email"]
+          email: raw_info["email"],
+          first_name: raw_info["first_name"],
+          last_name: raw_info["last_name"],
+          display_name: raw_info["display_name"],
+          job_title: raw_info["job_title"],
+          time_zone: raw_info["time_zone"],
+          country: raw_info["country"],
+          region: raw_info["region"],
+          company_name: raw_info["company_name"]
         }
       end
 
@@ -31,7 +38,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get("/me").parsed
+        @raw_info ||= access_token.get("/subscriber/info").parsed
       end
     end
   end
