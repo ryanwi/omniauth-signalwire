e9df67f2c4a759e134c8127a49e4460dec84499e
Ryan W <rwilliamz@gmail.com>
2024-02-28 20:57:54 -0800

    fix name error

--- lib/omniauth/strategies/signalwire.rb
+++ lib/omniauth/strategies/signalwire.rb
@@ -4,8 +4,35 @@ require "omniauth-oauth2"
 
 module OmniAuth
   module Strategies
-    class Signalwire
-      include OmniAuth::Strategy
+    class SignalWire < OmniAuth::Strategies::OAuth2
+      option :name, :signalwire
+
+      option :client_options, {
+        site: "https://fabric.signalwire.com",
+        authorize_url: "https://id.fabric.signalwire.com/login/oauth/authorize",
+        token_url: "https://id.fabric.signalwire.com/oauth/token"
+      }
+
+      option :pkce, true
+
+      uid { raw_info["id"] }
+
+      info do
+        {
+          name: raw_info["name"],
+          email: raw_info["email"]
+        }
+      end
+
+      extra do
+        {
+          "raw_info" => raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get("/me").parsed
+      end
     end
   end
 end
